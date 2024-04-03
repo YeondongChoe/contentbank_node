c70984e0b006a36437c1527d6a669f5d7c2f8b4e
yeondongChoe <ydchoi@olympiad.ac>
2024-04-03 14:41:29 +0900

    test

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(
 app.options("*", cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // 클라이언트의 주소
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // 클라이언트의 주소
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
