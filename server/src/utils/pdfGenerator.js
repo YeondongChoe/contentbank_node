046879e8487d940d80e7c0b5d2cac5c0f0c2f9f6
yeondongChoe <ydchoi@olympiad.ac>
2024-04-09 14:10:51 +0900

    수식변환 test

--- server/src/utils/pdfGenerator.js
+++ server/src/utils/pdfGenerator.js
@@ -61,14 +61,14 @@ async function generatePDF(data) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-      <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
       <!-- CSS 스타일 적용 -->
       <style>
         ${cssStyles}
       </style>
     </head>
     <body>
+      <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+      <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
       <div class="page">
         <div class="header">
           <div class="headerLeft">
@@ -107,13 +107,6 @@ async function generatePDF(data) {
         }
         </div>
       </div>
-      <script>
-        MathJax.typesetPromise()
-          .then(() => {
-            console.log("수식 렌더링 완료");
-          })
-          .catch((err) => console.error("수식 렌더링 에러", err));
-      </script>
     </body>
     </html>
   `,
