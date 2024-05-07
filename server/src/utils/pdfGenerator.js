bbcd000d933e23afa829f6a1c3592c0d1d973329
yeondongChoe <ydchoi@olympiad.ac>
2024-05-07 15:05:31 +0900

    workbook test

--- server/src/utils/pdfGenerator.js
+++ server/src/utils/pdfGenerator.js
@@ -74,43 +74,44 @@ async function generatePDF(data) {
   let pages = []; // 각 페이지의 HTML을 저장할 배열
 
   const generatePages = (questions) => {
-    let leftHtml = ""; // 좌측에 표시할 HTML 문자열
-    let rightHtml = ""; // 우측에 표시할 HTML 문자열
-    let totalHeight = 0;
-    let questionHeight = 200;
-    let leftPositionArray = [];
-    let rightPositionArray = [];
     let remainArray = [];
 
     questions.forEach((question) => {
+      let leftHtml = ""; // 좌측에 표시할 HTML 문자열
+      let rightHtml = ""; // 우측에 표시할 HTML 문자열
+      let totalHeight = 0;
+      let questionHeight = 200;
+      let leftPositionArray = [];
+      let rightPositionArray = [];
+
       // 높이가 900을 초과하지 않은 경우 좌측배열 추가
       if (totalHeight + questionHeight < 900) {
         // 좌측 배열에 추가
         leftPositionArray.push(question);
+        console.log("Left Position Array:", leftPositionArray);
+
+        // 좌측 HTML 구성
+        leftHtml = leftPositionArray
+          .map(
+            (question) =>
+              `<div class="left">문제 ${question.id}. ${question.content}</div>`
+          )
+          .join("");
+        console.log("leftHtml:", leftHtml);
+
         // 높이 추가
         totalHeight += questionHeight;
-        // 좌측과 우측 배열을 콘솔에 출력
         console.log("totalHeight:", totalHeight);
-        console.log("Left Position Array:", leftPositionArray);
       } else {
         //초과한 문제 우측 배열에 추가
         rightPositionArray.push(question);
         console.log("Right Position Array:", rightPositionArray);
       }
 
-      // 좌측 HTML 구성
-      leftHtml = leftPositionArray
-        .map(
-          (question) =>
-            `<div class="left">문제 ${question.id}. ${question.content}</div>`
-        )
-        .join("");
-
-      console.log("leftHtml:", leftHtml);
-
       // 우측 HTML 구성
       if (rightPositionArray.length > 0) {
-        //totalHeight = 0; // 높이 초기화
+        // 높이 초기화
+        totalHeight = 0;
         rightPositionArray.forEach((question) => {
           // 높이가 900을 초과하지 않는 경우 우측에 표시
           if (totalHeight + questionHeight < 900) {
@@ -121,8 +122,6 @@ async function generatePDF(data) {
             remainArray.push(question);
           }
         });
-        // 우측 HTML을 만든 후에 콘솔에 출력
-        console.log("Right HTML:", rightHtml);
       }
 
       // 페이지 HTML 구성
