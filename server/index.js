8301437ff9ae73880eec15a819f9e3e1a2ab3e8f
yeondongChoe <ydchoi@olympiad.ac>
2024-07-08 18:18:53 +0900

    힙 메모리 설정

--- server/index.js
+++ server/index.js
@@ -83,60 +83,19 @@ app.post("/get-pdf", async (req, res) => {
   // PDF 생성 모듈 호출
   const pdfBuffer = await generatePDF(data);
 
-  // 임시 파일 경로 (PDF 데이터를 임시 파일로 저장)
-  const tempFilePath = `${uploadDir}/temp_${fileName}`;
+  // 파일 저장 경로
+  const filePath = `${uploadDir}/${fileName}`;
 
-  // 임시 파일로 저장
-  fs.writeFile(tempFilePath, pdfBuffer, (err) => {
+  // 파일 저장
+  fs.writeFile(filePath, pdfBuffer, (err) => {
     if (err) {
-      console.error("임시 파일 저장 중 오류 발생:", err);
-      res.status(500).send("임시 파일 저장 중 오류가 발생했습니다.");
+      console.error("파일 저장 중 오류 발생:", err);
+      res.status(500).send("파일 저장 중 오류가 발생했습니다.");
     } else {
-      console.log("임시 파일이 성공적으로 저장되었습니다:", tempFilePath);
-
-      // 스트리밍 방식으로 파일 저장
-      const readStream = fs.createReadStream(tempFilePath);
-      const writeStream = fs.createWriteStream(`${uploadDir}/${fileName}`);
-
-      readStream.pipe(writeStream);
-
-      writeStream.on("finish", () => {
-        console.log(
-          "파일이 성공적으로 스트리밍되었습니다:",
-          `${uploadDir}/${fileName}`
-        );
-        res.send("파일이 성공적으로 스트리밍되었습니다.");
-
-        // 임시 파일 삭제
-        fs.unlink(tempFilePath, (err) => {
-          if (err) {
-            console.error("임시 파일 삭제 중 오류 발생:", err);
-          } else {
-            console.log("임시 파일이 성공적으로 삭제되었습니다.");
-          }
-        });
-      });
-
-      writeStream.on("error", (err) => {
-        console.error("파일 스트리밍 중 오류 발생:", err);
-        res.status(500).send("파일 스트리밍 중 오류가 발생했습니다.");
-      });
+      console.log("파일이 성공적으로 저장되었습니다:", filePath);
+      res.send("파일이 성공적으로 저장되었습니다.");
     }
   });
-
-  // 파일 저장 경로
-  // const filePath = `${uploadDir}/${fileName}`;
-
-  // // 파일 저장
-  // fs.writeFile(filePath, pdfBuffer, (err) => {
-  //   if (err) {
-  //     console.error("파일 저장 중 오류 발생:", err);
-  //     res.status(500).send("파일 저장 중 오류가 발생했습니다.");
-  //   } else {
-  //     console.log("파일이 성공적으로 저장되었습니다:", filePath);
-  //     res.send("파일이 성공적으로 저장되었습니다.");
-  //   }
-  // });
 });
 
 app.listen(port, () => {
